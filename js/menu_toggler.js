e3feba9e75aefc09fbaa0902b44129b04486828e
Matthias-GOUDJIL <matthias.goudjil@gmail.com>
2018-06-05 00:56:22 +0200

    add js effects

--- js/menu_toggler.js
+++ js/menu_toggler.js
@@ -1,7 +1,7 @@
 
     // Script to hide/show menu
-    let button = document.querySelector('#menu-toggler');
-    let   menu = document.querySelector('.menu-nav');
+    let button = document.querySelector('#menu_toggler');
+    let   menu = document.querySelector('#menu_nav');
 
     button.addEventListener('click', function (e) {
         menu.classList.toggle('hide')
