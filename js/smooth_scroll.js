fae9674246f285c5fa715a9b7d85a25daedb4ad6
Matthias-GOUDJIL <matthias.goudjil@gmail.com>
2018-06-08 01:58:33 +0200

    layout

--- js/smooth_scroll.js
+++ js/smooth_scroll.js
@@ -2,8 +2,8 @@
     // Select all links with hashes
     $('a[href*="#"]')
       // Remove links that don't actually link to anything
-      .not('[href="#"]')
-      .not('[href="#0"]')
+      // .not('[href="#"]')
+      // .not('[href="#0"]')
       .click(function(event) {
         // On-page links
         if (
