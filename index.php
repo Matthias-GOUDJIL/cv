deb5423a28264b00f52529057bfb7260f7156f19
Matthias-GOUDJIL <matthias.goudjil@gmail.com>
2018-07-24 22:50:15 +0200

    analytics tag

--- index.php
+++ index.php
@@ -10,7 +10,16 @@
 <html lang="fr">
 
 <head>
-    <!-- Google Tag Manager -->
+    <!-- Global site tag (gtag.js) -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=UA-122837117-1"></script>
+    <script>
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    gtag('js', new Date());
+
+    gtag('config', 'UA-122837117-1');
+    </script>
+    <!-- Tag Manager -->
     <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
     new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
     j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
@@ -18,7 +27,7 @@
     })(window,document,'script','dataLayer','GTM-WZPFRKC');</script>
     <!-- End Google Tag Manager -->
 
-    <!-- Balisage JSON-LD de données structurées de Google --> 
+    <!-- Balisage JSON-LD de données structurées --> 
     <script type="application/ld+json"> {
         "@context" : "http://schema.org",
         "@type" : "LocalBusiness",
